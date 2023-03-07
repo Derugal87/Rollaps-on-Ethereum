47234861f3c02691dbb9a8a413178321049527f2
Alexander Derugo <alexderugo@gmail.com>
2023-03-08 00:31:15 +0400

    rollap test

---
+++ zkEVM/zkevm-tutorial_hardhat/hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+const { PRIVATE_KEY, INFURA_API_KEY, ETHERSCAN_API_KEY } = process.env;
+
+// module.exports = {
+//   solidity: "0.8.18",
+//   networks: {
+//     consensyszkevmgoerli: {
+//       url: `https://consensys-zkevm-goerli-prealpha.infura.io/v3/${INFURA_API_KEY}`,
+//       accounts: [PRIVATE_KEY],
+//     },
+//   },
+// };
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    consensyszkevmgoerli: {
+      url: `https://consensys-zkevm-goerli-prealpha.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      consensyszkevmgoerli: ETHERSCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "consensyszkevmgoerli",
+        chainId: 59140,
+        urls: {
+          apiURL: "https://explorer.goerli.zkevm.consensys.net/api",
+          browserURL: "https://explorer.goerli.zkevm.consensys.net/"
+        }
+      }
+    ]
+  }
+};
